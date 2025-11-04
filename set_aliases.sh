2664fca79dbea22859d404c1adb432b92b68883b
mmauro2152 <mauro.mtz.2152@outlook.com>
2025-11-04 12:50:04 -0600

    updated compile & debug setup

--- set_aliases.sh
+++ set_aliases.sh
@@ -1,3 +1,3 @@
-alias compile='g++ -fdiagnostics-color=always -g VarTable.cpp FuncDir.cpp MochiLexer/MochiLexer.yy.cpp MochiParser/MochiParser.tab.cpp main.cpp -o mochi.exe'
-alias parser='bison -H -o MochiParser/MochiParser.tab.cpp MochiParser/MochiParser.y'
-alias lexer='flex --header-file=MochiLexer/MochiLexer.yy.hh -o MochiLexer/MochiLexer.yy.cpp MochiLexer/MochiLexer.l'
+alias compile='g++ -fdiagnostics-color=always -g src/VarTable.cpp src/FuncDir.cpp src/MochiLexer/MochiLexer.yy.cpp src/MochiParser/MochiParser.tab.cpp src/main.cpp -o mochi.exe'
+alias parser='bison -H -o src/MochiParser/MochiParser.tab.cpp src/MochiParser/MochiParser.y'
+alias lexer='flex --header-file=src/MochiLexer/MochiLexer.yy.hh -o src/MochiLexer/MochiLexer.yy.cpp src/MochiLexer/MochiLexer.l'
