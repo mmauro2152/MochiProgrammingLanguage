6eed4e94dff246546f7b49a513b5c53b045667f0
mmauro2152 <mauro.mtz.2152@outlook.com>
2025-11-05 14:08:01 -0600

    update compile and debug setup

--- set_aliases.sh
+++ set_aliases.sh
@@ -1,3 +1,3 @@
-alias compile='g++ -fdiagnostics-color=always -g src/SemanticCube.cpp src/VarTable.cpp src/FuncDir.cpp src/MochiLexer/MochiLexer.yy.cpp src/MochiParser/MochiParser.tab.cpp src/main.cpp -o mochi.exe'
+alias compile='g++ -fdiagnostics-color=always -g src/*.cpp src/Mochi*/*.*.cpp -o mochi.exe'
 alias parser='bison -H -o src/MochiParser/MochiParser.tab.cpp src/MochiParser/MochiParser.y'
 alias lexer='flex --header-file=src/MochiLexer/MochiLexer.yy.hh -o src/MochiLexer/MochiLexer.yy.cpp src/MochiLexer/MochiLexer.l'
