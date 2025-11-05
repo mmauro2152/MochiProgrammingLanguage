0113d020f7ad69a7257c6ed1fb4b57800e4a778e
mmauro2152 <mauro.mtz.2152@outlook.com>
2025-11-04 22:42:38 -0600

    update compilation setup

--- set_aliases.sh
+++ set_aliases.sh
@@ -1,3 +1,3 @@
-alias compile='g++ -fdiagnostics-color=always -g VarTable.cpp FuncDir.cpp MochiLexer/MochiLexer.yy.cpp MochiParser/MochiParser.tab.cpp main.cpp -o mochi.exe'
+alias compile='g++ -fdiagnostics-color=always -g SemanticCube.cpp VarTable.cpp FuncDir.cpp MochiLexer/MochiLexer.yy.cpp MochiParser/MochiParser.tab.cpp main.cpp -o mochi.exe'
 alias parser='bison -H -o MochiParser/MochiParser.tab.cpp MochiParser/MochiParser.y'
 alias lexer='flex --header-file=MochiLexer/MochiLexer.yy.hh -o MochiLexer/MochiLexer.yy.cpp MochiLexer/MochiLexer.l'
