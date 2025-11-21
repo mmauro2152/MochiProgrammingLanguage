d220679ba6d0a029f80847c1040270904c2f9ba6
mmauro2152 <mauro.mtz.2152@outlook>
2025-11-21 00:18:41 -0600

    refactor virtual memory

---
+++ src/proxyarr.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T>
+struct proxyarr {
+    T* arr;
+
+    T& operator[](int col) {
+        return arr[col];
+    }
+
+    template <typename enum_>
+    T& operator[](enum_ col) {
+        return arr[static_cast<int>(col)];
+    }
+
+    proxyarr(): arr(nullptr) {}
+    proxyarr(T* r): arr(r) {}
+    proxyarr(int len): arr(new T[len]) {}
+
+};
